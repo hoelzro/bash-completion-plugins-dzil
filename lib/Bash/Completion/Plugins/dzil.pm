2e2289b1153f23caa6ececdc4fdbcd8da3b3662d
Rob Hoelz <rob@hoelz.ro>
2012-09-02 21:54:52 +0200

    Add implementation

--- lib/Bash/Completion/Plugins/dzil.pm
+++ lib/Bash/Completion/Plugins/dzil.pm
@@ -4,6 +4,15 @@ package Bash::Completion::Plugins::dzil;
 ## use critic (RequireUseStrict)
 use strict;
 use warnings;
+use parent 'Bash::Completion::Plugins::App::Cmd';
+
+use Bash::Completion::Utils qw(command_in_path);
+
+sub should_complete {
+    return [ grep { command_in_path($_) } qw(dzil) ];
+}
+
+sub command_class { 'Dist::Zilla::App' }
 
 1;
 
