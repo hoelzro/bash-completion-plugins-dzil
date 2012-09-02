a88a76f6811fef7a454a397bfa230c6a4c32c062
Rob Hoelz <rob@hoelz.ro>
2012-09-02 21:26:21 +0200

    initial commit

---
+++ lib/Bash/Completion/Plugins/dzil.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Bash::Completion::Plugins::dzil;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of Bash::Completion::Plugins::dzil
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
